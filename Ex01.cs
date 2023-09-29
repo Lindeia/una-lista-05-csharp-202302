aead51e0ea7e9166b3c1f74f1b0870f6a316b896
Lindeia <90582252+Lindeia@users.noreply.github.com>
2023-09-29 14:30:26 -0300

    Add files via upload

---
+++ Ex01.cs
@@ -0,0 +1,14 @@
+﻿int[] numeros = new int[5];
+
+Console.WriteLine("Digite 5 números inteiros (pressionar Enter para cada número digitado)");
+
+for (int p = 0; p < numeros.Length; p++)
+{
+    numeros[p] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("\n\nEstes são os números que você digitou:");
+for (int p = 0; p < numeros.Length; p++)
+{
+    Console.WriteLine(numeros[p]);
+}
