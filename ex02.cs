ff57a7a5b4b4188e30e6b65c985429713147a408
Lindeia <90582252+Lindeia@users.noreply.github.com>
2023-10-02 12:17:39 -0300

    Add files via upload

---
+++ ex02.cs
@@ -0,0 +1,48 @@
+﻿Random rnd = new Random();
+int[] numeros = new int[10];
+//rnd = new Random();
+//static Random rnd;
+Preencher();
+
+Console.WriteLine("Esses são os números que escolhi: ");
+//int numeros = Convert.ToInt32(Console.ReadLine());
+Imprimir();
+Console.WriteLine();
+
+Console.WriteLine("A Ordem decrescente dos números que escolhi são: ");
+OrdenarDecrescente();
+Imprimir();
+Console.WriteLine();
+
+void Preencher()
+{
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        numeros[i] = rnd.Next(1, 100);
+
+    }
+}
+void OrdenarDecrescente()
+{
+    for (int i = 0; i < numeros.Length - 1; i++)
+    {
+        for (int j = i + 1; j < numeros.Length; j++)
+        {
+            if (numeros[i] < numeros[j])
+            {
+                int aux = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = aux;
+            }
+        }
+    }
+}
+
+void Imprimir()
+{
+    for (int i = 0; i < numeros.Length; i++)
+    {
+        Console.WriteLine("Posição: " + i + "\tValor: " + numeros[i]);
+
+    }
+}Console.ReadLine();
